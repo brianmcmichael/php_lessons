13cae3bc80a84644ecde8e7909f56b404f4579da
Brian L. McMichael <brian@brianmcmichael.com>
2013-02-28 22:27:16 -0500

    search engine

--- testsite/links.php
+++ testsite/links.php
@@ -4,5 +4,6 @@
             <a href="form.php">Register</a>&nbsp
             <a href="update.php">Edit User</a>&nbsp
             <a href="delete.php">Delete User</a>&nbsp
+            <a href="search.php">Search User</a>&nbsp
     </h3>
 </center>

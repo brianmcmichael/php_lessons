b4a56654947a4b17acc641cf9f763ab4bbd34e86
Brian L. McMichael <brian@brianmcmichael.com>
2013-03-01 22:43:12 -0500

    pagination

---
+++ testsite/pagination.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * User: Brian
+ * Date: 2/28/13
+ * Time: 10:27 PM, 2/28/13, 2013
+ */
+
+mysql_connect("localhost", "root", "") or die("problem with connection...");
+mysql_select_db("testsite");
+
+$per_page = 6;
+$pages_query = mysql_query("SELECT COUNT('id') FROM users");
+$pages = ceil(mysql_result($pages_query, 0) / $per_page);
+
+$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+$start = ($page - 1) * $per_page;
+$query = mysql_query("SELECT name FROM users LIMIT $start, $per_page");
+
+while($query_row = mysql_fetch_assoc($query))            {
+    echo $query_row['name'].'<br />';
+}
+
+$prev = $page -1;
+$next = $page +1;
+
+echo '<br />';
+
+if($page > 1){
+echo "<a href='pagination.php?page=$prev'>Prev</a> ";
+}
+
+if($pages >= 1){
+    for($x=1;$x<=$pages;$x++)    {
+        echo ($x == $page) ? '<strong><a href="?page='.$x.'">'.$x.'</a></strong> ' : '<a href="?page='.$x.'">'.$x.'</a> ';
+    }
+}
+if($page < $pages){
+echo "<a href='pagination.php?page=$next'>Next</a> ";
+}

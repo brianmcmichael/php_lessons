64475ce5f966137ae3a899d99c6f52b80ec2cb41
Brian L. McMichael <brian@brianmcmichael.com>
2013-03-11 22:13:41 -0400

    date & time

--- testsite/links.php
+++ testsite/links.php
@@ -6,4 +6,9 @@
             <a href="delete.php">Delete User</a>&nbsp
             <a href="search.php">Search User</a>&nbsp
     </h3>
+    <br />
+    <?php
+    $date = date('F d, Y, g:i:s a');
+    echo "Today is: ".$date;
+    ?>
 </center>

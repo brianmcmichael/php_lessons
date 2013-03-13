ee3a385fa01804ede915a557e54758404b368728
Brian L. McMichael <brian@brianmcmichael.com>
2013-03-12 23:53:09 -0400

    constants

---
+++ lessons/63ClassConstants.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * User: Brian
+ * Date: 3/12/13
+ * Time: 11:43 PM, 3/12/13, 2013
+ */
+
+class Constants{
+    const RED = "#FF0000";
+    const Hello = "Hello!";
+
+    function output(){
+        echo self::RED;
+    }
+
+
+}
+$c = new Constants();
+$c->output();
+
+echo "<br />".Constants::Hello;
